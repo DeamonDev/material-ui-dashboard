a7a19778d6386b79287959d4ed0131840617263f
Piotr Rudnicki <deamondev@gmail.com>
2022-07-25 08:58:29 +0200

    better layout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,29 @@
-
+import { Search } from "@mui/icons-material";
+import { AppBar, InputBase, Toolbar, Typography } from "@mui/material";
+import { Box } from "@mui/system";
 
 function Navbar() {
-
   return (
-    <p>Hello</p>
+    <AppBar>
+      <Toolbar sx={{ display: "flex", justifyContent: "start" }}>
+        <Typography variant="h6" sx={{ marginRight: "20px" }}>
+          DeamonDev
+        </Typography>
+        <Box sx={{ display: "flex", justifyContent: "center" }}>
+          <Search sx={{ marginRight: "2px" }} />
+          <InputBase
+            placeholder="search..."
+            sx={{
+              backgroundColor: "#18b9a5",
+              fullWidth: true,
+              borderRadius: "5px",
+            }}
+          />
+        </Box>
+        <Box sx={{width: "75%"}}></Box>
+        <Typography variant="h6">sync.io</Typography>
+      </Toolbar>
+    </AppBar>
   );
 }
 
