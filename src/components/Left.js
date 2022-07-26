bcfccd9d37a15b52452d5878339783106b9357d6
Piotr Rudnicki <deamondev@gmail.com>
2022-07-26 10:34:46 +0200

    more icons

--- src/components/Left.js
+++ src/components/Left.js
@@ -1,4 +1,4 @@
-import { Apps, Home } from "@mui/icons-material";
+import { Apps, ArrowBackIos, ArrowForward, Home } from "@mui/icons-material";
 import { Card, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 
@@ -8,10 +8,23 @@ const onMouseOnItem = (e) => {
 
 function Left() {
   return (
-    <Card sx={{ backgroundColor: "#0066ff", paddingLeft: -8, height: "100%", flexDirection: "column" }}>
+    <Card
+      sx={{
+        backgroundColor: "#0066ff",
+        paddingLeft: -8,
+        height: "100%",
+        flexDirection: "column",
+      }}
+    >
       <Typography
         variant="h6"
-        sx={{ marginLeft: 1, paddingTop: 3, paddingBottom: 3, color: "white", fontWeight: "bold" }}
+        sx={{
+          marginLeft: 1,
+          paddingTop: 3,
+          paddingBottom: 3,
+          color: "white",
+          fontWeight: "bold",
+        }}
       >
         Home
       </Typography>
@@ -23,9 +36,11 @@ function Left() {
           alignItems: "center",
           backgroundColor: "#0099cc",
           color: "white",
-          marginBottom: 1
+          marginBottom: 1,
+          "&:hover": {
+            backgroundColor: "green",
+          },
         }}
-        onMouseOver={onMouseOnItem}
       >
         <Home />
         <Typography variant="h6" sx={{ fontWeight: "bold" }}>
@@ -40,12 +55,33 @@ function Left() {
           alignItems: "center",
           backgroundColor: "#0099cc",
           color: "white",
+          marginBottom: 1,
+          "&:hover": {
+            backgroundColor: "green",
+          },
         }}
-        onMouseOver={onMouseOnItem}
       >
         <Apps />
         <Typography variant="h6" sx={{ fontWeight: "bold" }}>
           Apps
+        </Typography>
+      </Card>
+      <Card
+        sx={{
+          display: "flex",
+          justifyContent: "start",
+          gap: 2,
+          alignItems: "center",
+          backgroundColor: "#0099cc",
+          color: "white",
+          "&:hover": {
+            backgroundColor: "green",
+          },
+        }}
+      >
+        <ArrowForward />
+        <Typography variant="h6" sx={{ fontWeight: "bold" }}>
+          iOS
         </Typography>
       </Card>
     </Card>
